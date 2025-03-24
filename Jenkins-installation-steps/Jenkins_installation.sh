8a71a3d8a78449b5d4748ecc3f88a8781f49f5c9
ramak8sgcp <ramak8sgcp@gmail.com>
2025-03-24 08:48:37 +0530

    jenkinsfile

--- Jenkins-installation-steps/Jenkins_installation.sh
+++ Jenkins-installation-steps/Jenkins_installation.sh
@@ -1,4 +1,4 @@
-sudo wget -o /etc/yum.repos.d/jenkins.repo \
+sudo curl -o /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 #sudo dnf upgrade
