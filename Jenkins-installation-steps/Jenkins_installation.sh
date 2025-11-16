a00a553d925ccdd4ef5166477b1f6dfc11678035
ramak8sgcp <ramak8sgcp@gmail.com>
2025-11-16 19:16:16 +0530

    update

--- Jenkins-installation-steps/Jenkins_installation.sh
+++ Jenkins-installation-steps/Jenkins_installation.sh
@@ -6,7 +6,7 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo dnf install fontconfig java-17-openjdk -y
 sudo dnf install jenkins -y
 sudo systemctl daemon-reload
-sudo systemctl start jenkins &&
+sudo systemctl start jenkins 
 sudo systemctl enable jenkins
 
 
